61023df3bf86c2cfc068d0e4d045a098ff17482d
bacnh7427 <bacnh7427@co-well.com.vn>
2022-09-22 17:36:55 +0700

    add cònig

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"back-go-land/pkg/config"
 	"back-go-land/pkg/handlers"
+	"back-go-land/pkg/render"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 const PortNumber = ":8082"
 
 func main() {
+
+	var app config.AppConfig
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 	http.HandleFunc("/new", handlers.New)
