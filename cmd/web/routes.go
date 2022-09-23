b66a1e7f0efb5efe4168c92538b058ab1b422099
bacnh7427 <bacnh7427@co-well.com.vn>
2022-09-23 16:02:32 +0700

    developiing own middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,13 +5,26 @@ import (
 	"back-go-land/pkg/handlers"
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	mux.Get("/new", http.HandlerFunc(handlers.Repo.New))
+
+	// using pat for routing
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/new", http.HandlerFunc(handlers.Repo.New))
+
+	// using chi
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/new", handlers.Repo.New)
 	return mux
 }
